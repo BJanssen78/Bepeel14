1e91166e0072a415455f0a059af3b22218cb0440
Ben Janssen <98946596+BJanssen78@users.noreply.github.com>
2022-10-03 21:05:49 +0200

    Created js file superheroes

---
+++ superpower.js
@@ -0,0 +1,141 @@
+/*
+Dit stukje heb ik gejat van Winc Academy Leraar Niels.
+Een leuk hebbe dingetje die er gaaf en professioneel uitziet.
+Dank je wel Niels.
+*/
+(() => {
+    console.clear();
+    const currentdate = new Date();
+    console.log(
+        `>>> \x1b[32m💾 Bepeel Development \x1b[0m <<< ${
+            currentdate.getHours() +
+            ":" +
+            currentdate.getMinutes() +
+            ":" +
+            currentdate.getSeconds()
+        } ${ ">>> \x1b[32m Bepeel Development \x1b[0m <<<"}`
+    );
+})();//Einde hebbe dingetje van Niels.
+
+const superheroes = [
+    {
+    "name": "Batman",
+    "publisher": "DC Comics",
+    "alter_ego": "Bruce Wayne",
+    "first_appearance": "Detective Comics #27",
+    "weight": "210"
+    },
+    {
+    "name": "Superman",
+    "publisher": "DC Comics",
+    "alter_ego": "Kal-El",
+    "first_appearance": "Action Comics #1",
+    "weight": "220"
+    },
+    {
+    "name": "Flash",
+    "publisher": "DC Comics",
+    "alter_ego": "Jay Garrick",
+    "first_appearance": "Flash Comics #1",
+    "weight": "195"
+    },
+    {
+    "name": "Green Lantern",
+    "publisher": "DC Comics",
+    "alter_ego": "Alan Scott",
+    "first_appearance": "All-American Comics #16",
+    "weight": "186"
+    },
+    {
+    "name": "Green Arrow",
+    "publisher": "DC Comics",
+    "alter_ego": "Oliver Queen",
+    "first_appearance": "All-American Comics #16",
+    "weight": "195"
+    },
+    {
+    "name": "Wonder Woman",
+    "publisher": "DC Comics",
+    "alter_ego": "Princess Diana",
+    "first_appearance": "The Incredible Hulk #180",
+    "weight": "165"
+    },
+    {
+    "name": "Blue Beetle",
+    "publisher": "DC Comics",
+    "alter_ego": "Dan Garret",
+    "first_appearance": "Mystery Men Comics #1",
+    "weight": "145"
+    },
+    {
+    "name": "Spider Man",
+    "publisher": "Marvel Comics",
+    "alter_ego": "Peter Parker",
+    "first_appearance": "Amazing Fantasy #15",
+    "weight": "167"
+    },
+    {
+    "name": "Captain America",
+    "publisher": "Marvel Comics",
+    "alter_ego": "Steve Rogers",
+    "first_appearance": "Captain America Comics #1",
+    "weight": "220"
+    },
+    {
+    "name": "Iron Man",
+    "publisher": "Marvel Comics",
+    "alter_ego": "Tony Stark",
+    "first_appearance": "Tales of Suspense #39",
+    "weight": "250"
+    },
+    {
+    "name": "Thor",
+    "publisher": "Marvel Comics",
+    "alter_ego": "Thor Odinson",
+    "first_appearance": "Journey into Myster #83",
+    "weight": "200"
+    },
+    {
+    "name": "Hulk",
+    "publisher": "Marvel Comics",
+    "alter_ego": "Bruce Banner",
+    "first_appearance": "The Incredible Hulk #1",
+    "weight": "1400"
+    },
+    {
+    "name": "Wolverine",
+    "publisher": "Marvel Comics",
+    "alter_ego": "James Howlett",
+    "first_appearance": "The Incredible Hulk #180",
+    "weight": "200"
+    },
+    {
+    "name": "Daredevil",
+    "publisher": "Marvel Comics",
+    "alter_ego": "Matthew Michael Murdock",
+    "first_appearance": "Daredevil #1",
+    "weight": "200"
+    },
+    {
+    "name": "Silver Surfer",
+    "publisher": "Marvel Comics",
+    "alter_ego": "Norrin Radd",
+    "first_appearance": "The Fantastic Four #48",
+    "weight": "unknown"
+    }
+    ]
+
+    // console.log(superheroes);
+    // console.log(superheroes.map(heroName => heroName.name));
+    // console.log(superheroes.map(heroWeight => heroWeight.name, heroWeight.weight));
+    console.log(superheroes.map(heroName => heroName.weight, heroName.name));
+    // console.log(heroWeight(superheroes));
+    console.log(superheroes.map(heroWeight => {
+        let lightHero = [];
+        if (heroWeight.weight < 190){
+            lightHero.push(heroWeight.name);
+        }}       
+    ))
+
+    // note to myself.
+    // alles mappen, en in nieuwe array stoppen, voorbeeld zie arraymethods2.js
